ec0cd6144828fca1fe3d2d0f63f3236d412b0d2f
hyun671217 <hyun671217@daum.net>
2021-07-28 13:09:11 +0900

    캘린더

--- app/src/main/java/com/example/letsvegan/calender.java
+++ app/src/main/java/com/example/letsvegan/calender.java
@@ -10,5 +10,6 @@ public class calender extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
+        //HIHI
     }
 }
