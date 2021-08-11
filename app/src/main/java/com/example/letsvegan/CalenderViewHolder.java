8d4149c8bf909e28980298b9b688b40792e4c180
dew715 <dltmf1406@naver.com>
2021-08-11 15:34:31 +0900

    hhhhhh

--- app/src/main/java/com/example/letsvegan/CalenderViewHolder.java
+++ app/src/main/java/com/example/letsvegan/CalenderViewHolder.java
@@ -1,6 +1,26 @@
-package com.example.letsvegan;
+package codewithcal.au.calendarappexample;
 
-public class CalenderViewHolder {
+import android.view.View;
+import android.widget.TextView;
 
-    //1
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    public final TextView dayOfMonth;
+    private final CalendarAdapter.OnItemListener onItemListener;
+    public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener)
+    {
+        super(itemView);
+        dayOfMonth = itemView.findViewById(R.id.cellDayText);
+        this.onItemListener = onItemListener;
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        onItemListener.onItemClick(getAdapterPosition(), (String) dayOfMonth.getText());
+    }
 }
